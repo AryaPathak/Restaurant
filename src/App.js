c6c3a0e88808cb98f626a760fafe4e514dd6b4c5
AryaPathak <99078194+AryaPathak@users.noreply.github.com>
2023-06-02 01:46:12 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <MyName/>
+      Hello!!
+    </div>
+  )
+}
+
+const MyName = () => {
+  return <h1>Arya Pathak</h1>
+}
+
+export default App
+
